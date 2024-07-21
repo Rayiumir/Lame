ad4e610c7faf61e4346b2e8e97a7e56ab1a37225
Raymond Baghumian <swordart2050@gmail.com>
2024-07-22 02:25:06 +0330

    Writing Mobile Function for Mobile Form

--- src/Http/Channels/SMSChannel.php
+++ src/Http/Channels/SMSChannel.php
@@ -9,12 +9,14 @@ class SMSChannel
 {
     public function send($notifiable, Notification $notification): void
     {
+        return 'Done!';
+
         $receptor = $notifiable->mobile;
         $type = 1;
         $template = "Test";
         $param1 = $notification->code;
 
-        $api = new GhasedakApi(env('GHASEDAK_API_KEY'));
+        $api = new GhasedakApi(env('d8fdeaee912f55b43332764628808f33ba513326a7b557f63b36a3cacb401eb0'));
         $api->Verify($receptor, $type, $template, $param1);
     }
 }
--- src/Http/Controllers/auth/mobile/MobileController.php
+++ src/Http/Controllers/auth/mobile/MobileController.php
@@ -3,7 +3,10 @@
 namespace Rayium\Lame\Http\Controllers\auth\mobile;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Rayium\Lame\Http\Notifications\OTPSMS;
 use Rayium\Lame\Rules\ValidMobile;
 
 class MobileController extends Controller
@@ -18,5 +21,28 @@ class MobileController extends Controller
         $request->validate([
             'mobile' => ['required', new ValidMobile()]
         ]);
+
+        try {
+
+            $user = User::where('mobile', $request->mobile)->first();
+            $OTPCode = mt_rand(100000, 999999);
+            $loginToken = Hash::make('DCDCojncd@cdjn%!!ghnjrgtn&&');
+
+            if ($user) {
+                $user->update([
+                    'login_token' => $loginToken
+                ]);
+            } else {
+                $user = User::query()->create([
+                    'mobile' => $request->mobile,
+                    'login_token' => $loginToken
+                ]);
+            }
+            $user->notify(new OTPSMS($OTPCode));
+
+            return response(['login_token' => $loginToken], 200);
+        } catch (\Exception $ex) {
+            return response(['errors' => $ex->getMessage()], 422);
+        }
     }
 }
